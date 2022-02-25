b7b1a1a62baafe1bd7c49d41b50adc12111a8368
BeepSterr <git@beeps.dev>
2022-02-26 00:35:49 +0100

    Tests!

---
+++ tests/User.js
@@ -0,0 +1,36 @@
+const Storable = require("../utilities/storable");
+const {customAlphabet} = require("nanoid");
+module.exports = class User extends Storable {
+
+    static get table(){
+        return 'user';
+    }
+
+
+    /**
+     *
+     * @param Connector ../base.js
+     * @returns {[{field: string, name: string, type: *, nullable: ?boolean, references: ?Storable, reference_field: ?String, primary: ?Boolean}]}
+     */
+    static defineColumns(Connector){
+        return [
+            { name: 'username', field: 'username',type: Connector.types[Connector.types.string], primary: false },
+            ...super.defineColumns(Connector)
+        ];
+    }
+
+    #username;
+    get username(){
+        return this.#username;
+    }
+
+    set username(v){
+        this.changed = true;
+        this.#username = v;
+    }
+
+    toString(){
+        return `@${this.username}`
+    }
+
+}
---
+++ tests/collection.test.js
@@ -0,0 +1,38 @@
+const Collection = require('../utilities/collection');
+const User = require("./User");
+
+class CorrectType {
+    static a = 0;
+}
+class WrongType {
+    static a = 1;
+}
+
+describe('Collections', () => {
+
+    test('instanceof self', () => {
+        const x = new Collection(CorrectType);
+        expect(x instanceof Collection).toBe(true);
+    });
+
+    test('Collection.first', () => {
+        const x = new Collection(User);
+        const y = new User(1);
+        const z = new User(2);
+
+        x.set(1, y);
+        x.set(2, z);
+        expect(x.first).toBe(y);
+    });
+
+    test('Collection.last', () => {
+        const x = new Collection(User);
+        const y = new User(1);
+        const z = new User(2);
+
+        x.set(1, y);
+        x.set(2, z);
+        expect(x.last).toBe(z);
+    });
+
+});
