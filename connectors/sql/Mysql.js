e1f7257a2e0eaf9983fd32275b14386e944a5d54
BeepSterr <git@beeps.dev>
2024-04-16 20:40:40 +0200

    wufck

--- connectors/sql/Mysql.js
+++ connectors/sql/Mysql.js
@@ -9,6 +9,29 @@ export default class Mysql extends SqlBased {
     }
 
     createDatabase(opts){
+
+        let extraOptions = {};
+        if(typeof opts.onSlowQuery === 'function'){
+            extraOptions = {
+                debug: true, // Enable debug to potentially catch more detailed info
+                log: {
+                    debug(message) {
+                        if (message.sql) {
+                            const queryStartTime = process.hrtime.bigint(); // Get start time in nanoseconds
+                            this.client.raw(message.sql, message.bindings).then(() => {
+                                const queryEndTime = process.hrtime.bigint(); // Get end time in nanoseconds
+                                const queryDuration = Number(queryEndTime - queryStartTime) / 1e6; // Convert to milliseconds
+                                if (queryDuration > 200) { // Define your threshold for slow queries
+                                    opts.onSlowQuery(message.sql, queryDuration)
+                                }
+                            }).catch(console.error);
+                        }
+                    }
+                }
+            }
+        }
+
+
         this.connection = new Knex({
             client: opts.client || 'mysql',
             connection: {
@@ -19,7 +42,8 @@ export default class Mysql extends SqlBased {
                 database : opts.db
             },
             useNullAsDefault: true,
-            postProcessResponse: this.sqlCollectionBuilder
+            postProcessResponse: this.sqlCollectionBuilder,
+            ...extraOptions
         });
 
     }
